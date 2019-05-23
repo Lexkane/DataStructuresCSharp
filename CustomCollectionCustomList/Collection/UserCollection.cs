dcaa4792be64cc12248336cca52ddf5120887932
Lexkane <40840623+Lexkane@users.noreply.github.com>
2019-05-23 17:56:26 +0300

    Add files via upload

---
+++ CustomCollectionCustomList/Collection/UserCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+
+
+namespace Collection
+{
+    class UserCollection : ICollection
+    {
+        readonly object syncRoot = new object();
+
+        readonly object[] elements = { 1, 2, 3, 4 };
+        
+        
+        public void CopyTo(Array array, int userArrayIndex)
+        {
+            var arr = array as object[];
+
+            if (arr == null)
+                throw new ArgumentException("Expecting array to be object[]");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[userArrayIndex++] = elements[i];
+            }
+        }
+
+       
+        public int Count
+        {
+            get { return elements.Length; }
+        }
+
+       
+        public bool IsSynchronized
+        {
+            get { return true; }
+        }
+
+        
+        public object SyncRoot
+        {
+            get { return syncRoot; }
+        }
+
+     
+        public IEnumerator GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }
+    }
+}
---
+++ CustomCollectionCustomList/Collection2/UserCollection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// ICollection<T> - определяет методы, используемые для управления универсальными коллекциями.
+
+namespace Collection
+{
+    class UserCollection<T> : ICollection<T>
+    {
+        T[] elements = new T[0];
+
+        
+        public void Add(T item)
+        {
+            var newArray = new T[elements.Length + 1]; 
+            elements.CopyTo(newArray, 0);             
+            newArray[newArray.Length - 1] = item;      
+            elements = newArray;                       
+        }
+
+        
+        public void Clear()
+        {
+            elements = new T[0];
+        }
+
+        
+        public bool Contains(T item)
+        {
+            foreach (var element in elements)
+            {
+                if (element.Equals(item))
+                    return true;
+            }
+
+            return false; 
+
+           
+            return elements.Contains(item);
+        }
+
+        
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            elements.CopyTo(array, arrayIndex);
+        }
+
+       
+        public int Count
+        {
+            get { return elements.Length; }
+        }
+
+      
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+       
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>) elements).GetEnumerator();
+        }
+
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (this as IEnumerable<T>).GetEnumerator();
+        }
+    }
+}
